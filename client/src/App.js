9e9940e3ed10dd6e7f1b0e15b61d5938bddd27d2
Steven Del Rosario <stevendelro@pm.me>
2021-08-10 12:07:08 -0500

    added /signup to app routes to render SignupLogin.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from '../src/pages/Home'
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignupLogin from '../src/pages/SignupLogin';
+import Home from '../src/pages/Home';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+  app_root: {
+    height: '100vh',
+  },
+}));
 
 export default function App() {
+  const classes = useStyles();
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </Router>
+    <div className={classes.app_root}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/signup" component={SignupLogin} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
